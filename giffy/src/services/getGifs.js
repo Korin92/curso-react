c09038eef45acb0c981a3094eddac24720b59b48
Korin92 <carmene8992@gmail.com>
2021-12-14 18:20:52 +0100

    feat/lazy-load-suspense

--- giffy/src/services/getGifs.js
+++ giffy/src/services/getGifs.js
@@ -14,9 +14,9 @@ const fromApiresponseToGifs = apiResponse => {
     return [];
 }
 
-export default function getGifs({keyword = 'morty'} = {}) {
+export default function getGifs({limit= 25, keyword = 'morty', page=0} = {}) {
 
-    const apiUrl = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=25&offset=0&rating=g&lang=en`;
+    const apiUrl = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=g&lang=en`;
 
     return fetch(apiUrl).then(res => res.json()).then(fromApiresponseToGifs)
 
