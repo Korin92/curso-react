77b10109611af67a7b3ac2d72b654bb43eb02cd4
Korin92 <carmene8992@gmail.com>
2021-12-16 18:26:45 +0100

    test

--- giffy/src/pages/Home/index.js
+++ giffy/src/pages/Home/index.js
@@ -36,16 +36,6 @@ export default function Home() {
                     <TrendingSearches />
                 </div>
             </div>
-
-            {/* <h3 className="App-title">Los gifs más populares</h3>
-            <ul>
-                {POPULAR_GIFS.map((popularGif) => (
-
-                    <li key={popularGif}>
-                        <Link to={`/search/${popularGif}`}>Gifs de {popularGif}</Link>
-                    </li>
-                ))}
-            </ul> */}
         </>
     )
 }
