e456277ce1636137b52d397e1f27f209cd4083ee
Korin92 <carmene8992@gmail.com>
2021-12-16 11:10:40 +0100

    reinstall

--- giffy/src/pages/SearchResults/index.js
+++ giffy/src/pages/SearchResults/index.js
@@ -1,5 +1,4 @@
-import React, { useRef } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import React, { useRef, useEffect } from 'react';
 import ListOfGifs from '../../components/ListOfGifs';
 import Spinner from '../../components/Spinner';
 import { useGifs } from '../../hooks/useGifs';
