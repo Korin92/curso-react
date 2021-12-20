34d7e1b46dde457600e9485e54d9fe0ada7f0132
Korin92 <carmene8992@gmail.com>
2021-12-20 17:59:55 +0100

    change-row

--- giffy/src/components/Gif/index.js
+++ giffy/src/components/Gif/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Gif.css';
+import { Link } from 'wouter';
 
-function Gif({title, id, url}) {
+function Gif({ title, id, url }) {
     return (
-        <a href={`#${id}`} className='Gif'>
-            <h4>{title}</h4>
-            <img alt={title} src={url} />
-        </a >
+        <div className="Gif">
+            <Link to={`/gif/${id}`} className='Gif-link'>
+                <h4>{title}</h4>
+                <img loading='lazy' alt={title} src={url} />
+            </Link>
+        </div>
     )
 }
 
-export default React.memo(Gif, (prevProps, nextProps) =>{
+export default React.memo(Gif, (prevProps, nextProps) => {
     return prevProps.id === nextProps.id
 })
