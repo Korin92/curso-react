7e5a8333c8f0321a3b61e3b7ae0c884fac328fea
Korin92 <carmene8992@gmail.com>
2021-12-21 11:51:22 +0100

    feat/SEO

--- giffy/src/pages/Home/index.js
+++ giffy/src/pages/Home/index.js
@@ -4,13 +4,14 @@ import ListOfGifs from "../../components/ListOfGifs";
 import { useGifs } from "../../hooks/useGifs";
 import TrendingSearches from "../../components/TrendingSearches";
 import SearchForm from "../../components/SearchForm";
+import { Helmet } from 'react-helmet';
 
 
 export default function Home() {
 
-    const [path, pushLocation] = useLocation();
+    const [_, pushLocation] = useLocation();
 
-    const { loading, gifs } = useGifs();
+    const { gifs } = useGifs();
 
     const handleSubmit = useCallback(({ keyword }) => {
 
@@ -20,14 +21,21 @@ export default function Home() {
 
     return (
         <>
-            <SearchForm onSubmit={handleSubmit} />
-            <div className="App-main">
-                <div className="App-results">
-                    <h3 className="App-title">Última búsqueda</h3>
-                    <ListOfGifs gifs={gifs} />
-                </div>
-                <div className="App-category">
-                    <TrendingSearches />
+        <Helmet>
+            <title>Home | Giffy</title>
+        </Helmet>
+            <header className="o-header">
+                <SearchForm onSubmit={handleSubmit} />
+            </header>
+            <div className="App-wrapper">
+                <div className="App-main">
+                    <div className="App-results">
+                        <h3 className="App-title">Última búsqueda</h3>
+                        <ListOfGifs gifs={gifs} />
+                    </div>
+                    <div className="App-category">
+                        <TrendingSearches />
+                    </div>
                 </div>
             </div>
         </>
