ac179ef2b96994f79ceb391212b99e1ccf8c23d2
Korin92 <carmene8992@gmail.com>
2022-01-03 13:49:23 +0100

    feat/styled-and-emotion

--- giffy/src/components/Category/index.js
+++ giffy/src/components/Category/index.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { Link } from "wouter";
+
+import {
+  CategoryTitle,
+  CategoryListItem,
+  CategoryLink,
+  CategoryList,
+} from "./styles";
 
 import "./Category.css";
 
 export default function Category({ name, options = [] }) {
-    return (
-        <section>
-            <div className="Category">
-                <h3 className="Category-title">{name}</h3>
-                <ul className="Category-List">
-                    {options.map((singleOption) => (
-                        <li key={singleOption}><Link className="Category-link"
-                            to={`/search/${singleOption}`}>{singleOption}
-                        </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    );
+  return (
+    <section>
+      <CategoryTitle>{name}</CategoryTitle>
+      <CategoryList>
+        {options.map((singleOption, index) => (
+          <CategoryListItem key={singleOption} index={index} type="primary">
+            <CategoryLink to={`/search/${singleOption}`}>
+              {singleOption}
+            </CategoryLink>
+          </CategoryListItem>
+        ))}
+      </CategoryList>
+    </section>
+  );
 }
